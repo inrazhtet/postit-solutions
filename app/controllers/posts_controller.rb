603f737036f022767a8f672da0eaa9a2f0ac90b7
ZarniHtet <zarnihtet@zarnihtet.com>
2014-03-26 21:13:05 -0700

    All done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id]) 
+  end
+
+  def new
 
   end
+
+  def create
+
+ 	end
+
+ 	def edit 
+
+ 	end
+
+ 	def update
+
+ 	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313062346) do
+ActiveRecord::Schema.define(version: 20140316215737) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20140313062346) do
     t.text     "body"
     t.integer  "post_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
